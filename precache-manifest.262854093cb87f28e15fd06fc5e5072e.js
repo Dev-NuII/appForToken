fda03837aa2b94543eda858e1108a8cc5d787e6b
root <root@smmuch.ru>
2020-02-14 16:14:02 +0300

    Updates

---
+++ precache-manifest.262854093cb87f28e15fd06fc5e5072e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "106c4797fe28970878685c4296d0fb5b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0d3c7048ee5acb3a6242",
+    "url": "./static/css/2.ae2f4c93.chunk.css"
+  },
+  {
+    "revision": "00e1f1339721b6bbb02e",
+    "url": "./static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "0d3c7048ee5acb3a6242",
+    "url": "./static/js/2.8e41f6ca.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "./static/js/2.8e41f6ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00e1f1339721b6bbb02e",
+    "url": "./static/js/main.b902f977.chunk.js"
+  },
+  {
+    "revision": "84e90a24f14302b34aae",
+    "url": "./static/js/runtime-main.b5570317.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  }
+]);
